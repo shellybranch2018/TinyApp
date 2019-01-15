07a43da476d5a78b37bd0904ec376826590c4813
Shelly Branch <sbranch@homeaway.com>
2019-01-15 19:07:26 +0000

    adding additional supporting files to build the app

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var app = express();
-var PORT = 8080; // default port 8080
+var PORT = 3000; // default port 8080
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,11 +12,17 @@ var urlDatabase = {
 };
 
 
-
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+
 
 
 
