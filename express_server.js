de25cbc519ed7c6411812df03d0ec87fe04a4106
Shelly Branch <sbranch@homeaway.com>
2019-01-16 17:09:41 +0000

    updated the app up to updating the database and sendig the user to a page with their new url and old url. Almost ready for theExpress part of the project.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var PORT = 3000; // default port 8080
+var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
@@ -12,15 +12,63 @@ var urlDatabase = {
 };
 
 
+function generateRandomString() {
+var alphaString = Math.random().toString(32).replace('0.', '');
+
+return alphaString.slice(0,6);
+}
+
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  var shortURL = generateRandomString();
+  var longURL = req.body.longURL;
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+
+  let templateVars = { 
+    shortURL: req.params.id,
+    urls: urlDatabase,
+    longURL: urlDatabase[req.params.id]
+    };
+
+//let templateVars = { urls: urlDatabase };
+  
+
+//console.log(postlongName + "test2")
+
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+
+let longURL = urlDatabase[req.params.shortURL];
+
+
+  res.redirect(longURL);
+});
+
+// This takes the full url and whatever is submitted is sent to the urls_show page.
+
+app.post("/urls", (req, res) => {
+  var shortURL = generateRandomString();
+  var longURL = req.body.longURL;
+
+  urlDatabase[shortURL] = longURL;
+  //console.log(urlDatabase);
+  console.log(req.body);  // debug statement to see POST parameters
+  //console.log("/urls/" + shortURL)
+  res.redirect("/urls/" + shortURL);
+
+});
+
+
 
 
 
